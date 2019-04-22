05003ca73baf3d9fcef572615edc42750340b513
Carl Graff <graffcarl@gmail.com>
2019-04-21 17:03:18 -0700

    Add notify-user

---
+++ functions/notify-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const _          = require('lodash');
+const getRecords = require('../lib/kinesis').getRecords;
+const AWS        = require('aws-sdk');
+const kinesis    = new AWS.Kinesis();
+const sns        = new AWS.SNS();
+const streamName = process.env.order_events_stream;
+const topicArn   = process.env.user_notification_topic;
+
+module.exports.handler = async (event, context) => {
+  const records = getRecords(event);
+  const orderAccepted = records.filter(r => r.eventType === 'order_accepted');
+
+  for (const order of orderAccepted) {
+    const snsReq = {
+      Message: JSON.stringify(order),
+      TopicArn: topicArn
+    };
+    await sns.publish(snsReq).promise();
+    console.log(`notified user [${order.userEmail}] of order [${order.orderId}] being accepted`);
+
+    const data = _.clone(order);
+    data.eventType = 'user_notified';
+
+    const kinesisReq = {
+      Data: JSON.stringify(data), // the SDK would base64 encode this for us
+      PartitionKey: order.orderId,
+      StreamName: streamName
+    };
+    await kinesis.putRecord(kinesisReq).promise();
+    console.log(`published 'user_notified' event to Kinesis`);
+  }
+  
+  return 'all done';
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+To add tagged version to github:
+git tag // to show exising tags e.g. 1.05.2
+git tag -a v1.05.2 -m "Production Serverless Workshop up to 06 + notify-restaurant" // to add local tag
+git push origin v1.05.2 // to push tag to github
